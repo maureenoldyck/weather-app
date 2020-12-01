19694e0853da79b1879fc675a1f7bc16ca3f3165
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-12-01 19:46:22 +0100

    Change template into normal div

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 
     // Declaration of variables 
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const content = document.querySelector(".collapsButton").nextElementSibling;
 
 
 
@@ -21,16 +22,12 @@
             .then(
                 ((response) => {
                     response.json().then((weatherInfo => {
-                        let template = document.querySelector("#weather-card");
-                        let target = document.querySelector("#target");
-                        let weatherData = template.content.cloneNode(true);
-                        weatherData.querySelector(".temperature").innerHTML = Math.round(weatherInfo.main.temp) + "°C";
-                        weatherData.querySelector(".city-name").innerHTML = weatherInfo.name;
-                        weatherData.querySelector(".description").innerHTML = weatherInfo.weather[0].main;
-                        weatherData.querySelector(".highest").innerHTML = Math.round(weatherInfo.main.temp_max) + "°C";
-                        weatherData.querySelector(".lowest").innerHTML = Math.round(weatherInfo.main.temp_min) + "°C";
-                        weatherData.querySelector(".details").innerHTML = weatherInfo.weather[0].description;
-                        target.appendChild(weatherData);
+                        document.querySelector(".temperature").innerHTML = Math.round(weatherInfo.main.temp) + "°C";
+                        document.querySelector(".city-name").innerHTML = weatherInfo.name;
+                        document.querySelector(".description").innerHTML = weatherInfo.weather[0].main;
+                        document.querySelector(".highest").innerHTML = Math.round(weatherInfo.main.temp_max) + "°C";
+                        document.querySelector(".lowest").innerHTML = Math.round(weatherInfo.main.temp_min) + "°C";
+                        document.querySelector(".details").innerHTML = weatherInfo.weather[0].description;
                     }));
 
                 })
@@ -42,40 +39,36 @@
             .then(
                 ((response) => {
                     response.json().then((weatherInfo => {
-                        let template = document.querySelector("#weather-card");
-                        let target = document.querySelector("#target");
-                        let weatherData = template.content.cloneNode(true);
-                        weatherData.querySelector(".tomorrow").innerHTML = days[(new Date(weatherInfo.list[8].dt_txt)).getDay()];
+                        document.querySelector(".tomorrow").innerHTML = days[(new Date(weatherInfo.list[8].dt_txt)).getDay()];
                         // Using list[8] gives me the temperature of that date at 12:00 PM, to get the temp for the same hour each day add 8 (except day 5 will be the temp of 09:00 AM) 
-                        weatherData.querySelector(".tomorrowTemperature").innerHTML = Math.round(weatherInfo.list[8].main.temp) + "°C";
+                        document.querySelector(".tomorrowTemperature").innerHTML = Math.round(weatherInfo.list[8].main.temp) + "°C";
                         // Using the dt_text to get the day of the week
-                        weatherData.querySelector(".dayAfterTomorrow").innerHTML = days[(new Date(weatherInfo.list[16].dt_txt)).getDay()];
-                        weatherData.querySelector(".dayAfterTomorrowTemperature").innerHTML = Math.round(weatherInfo.list[16].main.temp) + "°C";
-                        weatherData.querySelector(".inThreeDays").innerHTML = days[(new Date(weatherInfo.list[24].dt_txt)).getDay()];
-                        weatherData.querySelector(".inThreeDaysTemperature").innerHTML = Math.round(weatherInfo.list[24].main.temp) + "°C";
-                        weatherData.querySelector(".inFourDays").innerHTML = days[(new Date(weatherInfo.list[32].dt_txt)).getDay()];
-                        weatherData.querySelector(".inFourDaysTemperature").innerHTML = Math.round(weatherInfo.list[32].main.temp) + "°C";
-                        weatherData.querySelector(".inFiveDays").innerHTML = days[(new Date(weatherInfo.list[39].dt_txt)).getDay()];
-                        weatherData.querySelector(".inFiveDaysTemperature").innerHTML = Math.round(weatherInfo.list[39].main.temp) + "°C";
-
-                        target.appendChild(weatherData);
-
+                        document.querySelector(".dayAfterTomorrow").innerHTML = days[(new Date(weatherInfo.list[16].dt_txt)).getDay()];
+                        document.querySelector(".dayAfterTomorrowTemperature").innerHTML = Math.round(weatherInfo.list[16].main.temp) + "°C";
+                        document.querySelector(".inThreeDays").innerHTML = days[(new Date(weatherInfo.list[24].dt_txt)).getDay()];
+                        document.querySelector(".inThreeDaysTemperature").innerHTML = Math.round(weatherInfo.list[24].main.temp) + "°C";
+                        document.querySelector(".inFourDays").innerHTML = days[(new Date(weatherInfo.list[32].dt_txt)).getDay()];
+                        document.querySelector(".inFourDaysTemperature").innerHTML = Math.round(weatherInfo.list[32].main.temp) + "°C";
+                        document.querySelector(".inFiveDays").innerHTML = days[(new Date(weatherInfo.list[39].dt_txt)).getDay()];
+                        document.querySelector(".inFiveDaysTemperature").innerHTML = Math.round(weatherInfo.list[39].main.temp) + "°C";
                     }));
 
                 })
 
             );;
 
+        content.style.maxHeight = 0;
+
     });
 
     // Collapsible header function
+
     document.querySelector(".collapsButton").addEventListener("click", () => {
         document.querySelector(".collapsButton").classList.toggle("collapsButton--active");
-        const content = document.querySelector(".collapsButton").nextElementSibling;
         if (document.querySelector(".collapsButton").classList.contains("collapsButton--active")) {
             content.style.maxHeight = content.scrollHeight + "px";
         } else {
-            content.style.maxHeight = 0; 
+            content.style.maxHeight = 0;
         }
     });
 
