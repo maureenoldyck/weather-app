a6a5cd5e2065d82c242f8d84ed2b496d20a000df
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-12-04 10:24:10 +0100

    Change images link

--- script.js
+++ script.js
@@ -24,22 +24,22 @@
                             document.querySelector(".description").innerHTML = locationInfo.daily[0].weather[0].main;
                             document.querySelector(".highest").innerHTML = "<i class='fas fa-caret-up'></i>" + Math.round(locationInfo.daily[0].temp.max) + "°C";
                             document.querySelector(".lowest").innerHTML = "<i class='fas fa-caret-down'></i>" + Math.round(locationInfo.daily[0].temp.min) + "°C";
-                            document.querySelector(".weather-image").src = "images/" + locationInfo.daily[0].weather[0].main + ".png";
+                            document.querySelector(".weather-image").src = "./images/" + locationInfo.daily[0].weather[0].main + ".png";
                             document.querySelector(".tomorrow").innerHTML = "Tomorrow";
                             document.querySelector(".tomorrowTemperature").innerHTML = Math.round(locationInfo.daily[1].temp.day) + "°C";
-                            document.querySelector(".weather-icon-tomorrow").src = "images/" + locationInfo.daily[1].weather[0].main + ".png";
+                            document.querySelector(".weather-icon-tomorrow").src = "./images/" + locationInfo.daily[1].weather[0].main + ".png";
                             document.querySelector(".dayAfterTomorrow").innerHTML = "In Two Days";
                             document.querySelector(".dayAfterTomorrowTemperature").innerHTML = Math.round(locationInfo.daily[2].temp.day) + "°C";
-                            document.querySelector(".weather-icon-dayAfterTomorrow").src = "images/" + locationInfo.daily[2].weather[0].main + ".png";
+                            document.querySelector(".weather-icon-dayAfterTomorrow").src = "./images/" + locationInfo.daily[2].weather[0].main + ".png";
                             document.querySelector(".inThreeDays").innerHTML = "In Three Days";
                             document.querySelector(".inThreeDaysTemperature").innerHTML = Math.round(locationInfo.daily[3].temp.day) + "°C";
-                            document.querySelector(".weather-icon-inThreeDays").src = "images/" + locationInfo.daily[3].weather[0].main + ".png";
+                            document.querySelector(".weather-icon-inThreeDays").src = "./images/" + locationInfo.daily[3].weather[0].main + ".png";
                             document.querySelector(".inFourDays").innerHTML = "In Four Days";
                             document.querySelector(".inFourDaysTemperature").innerHTML = Math.round(locationInfo.daily[4].temp.day) + "°C";
-                            document.querySelector(".weather-icon-inFourDays").src = "images/" + locationInfo.daily[4].weather[0].main + ".png";
+                            document.querySelector(".weather-icon-inFourDays").src = "./images/" + locationInfo.daily[4].weather[0].main + ".png";
                             document.querySelector(".inFiveDays").innerHTML = "In Five Days";
                             document.querySelector(".inFiveDaysTemperature").innerHTML = Math.round(locationInfo.daily[5].temp.day) + "°C";
-                            document.querySelector(".weather-icon-inFiveDays").src = "images/" + locationInfo.daily[5].weather[0].main + ".png";
+                            document.querySelector(".weather-icon-inFiveDays").src = "./images/" + locationInfo.daily[5].weather[0].main + ".png";
 
                         }));
                     });;
@@ -143,19 +143,19 @@
 
                         document.querySelector(".tomorrow").innerHTML = days[(new Date(dayArray[0].dt_txt)).getDay()];
                         document.querySelector(".tomorrowTemperature").innerHTML = Math.round(dayArray[0].main.temp) + "°C";
-                        document.querySelector(".weather-icon-tomorrow").src = "images/" + dayArray[0].weather[0].main + ".png";
+                        document.querySelector(".weather-icon-tomorrow").src = "./images/" + dayArray[0].weather[0].main + ".png";
                         document.querySelector(".dayAfterTomorrow").innerHTML = days[(new Date(dayArray[1].dt_txt)).getDay()];
                         document.querySelector(".dayAfterTomorrowTemperature").innerHTML = Math.round(dayArray[1].main.temp) + "°C";
-                        document.querySelector(".weather-icon-dayAfterTomorrow").src = "images/" + dayArray[1].weather[0].main + ".png";
+                        document.querySelector(".weather-icon-dayAfterTomorrow").src = "./images/" + dayArray[1].weather[0].main + ".png";
                         document.querySelector(".inThreeDays").innerHTML = days[(new Date(dayArray[2].dt_txt)).getDay()];
                         document.querySelector(".inThreeDaysTemperature").innerHTML = Math.round(dayArray[2].main.temp) + "°C";
-                        document.querySelector(".weather-icon-inThreeDays").src = "images/" + dayArray[2].weather[0].main + ".png";
+                        document.querySelector(".weather-icon-inThreeDays").src = "./images/" + dayArray[2].weather[0].main + ".png";
                         document.querySelector(".inFourDays").innerHTML = days[(new Date(dayArray[3].dt_txt)).getDay()];
                         document.querySelector(".inFourDaysTemperature").innerHTML = Math.round(dayArray[3].main.temp) + "°C";
-                        document.querySelector(".weather-icon-inFourDays").src = "images/" + dayArray[3].weather[0].main + ".png";
+                        document.querySelector(".weather-icon-inFourDays").src = "./images/" + dayArray[3].weather[0].main + ".png";
                         document.querySelector(".inFiveDays").innerHTML = days[(new Date(dayArray[4].dt_txt)).getDay()];
                         document.querySelector(".inFiveDaysTemperature").innerHTML = Math.round(dayArray[4].main.temp) + "°C";
-                        document.querySelector(".weather-icon-inFiveDays").src = "images/" + dayArray[4].weather[0].main + ".png";
+                        document.querySelector(".weather-icon-inFiveDays").src = "./images/" + dayArray[4].weather[0].main + ".png";
 
 
 
